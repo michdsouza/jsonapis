eb16df4749572c75621b19f8f81a4799d078c868
Michelle D'Souza <michd2005@gmail.com>
2013-10-14 18:21:04 -0600

    Set up rails, gems, databse and rvm

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+ruby "2.0.0"
+
+# core
+gem "rails", "3.2.14"
+gem "pg"
+gem 'jquery-rails'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ab435d00c900fad917dc681486d0b41ad6c82d09948d3ce1e49351d4ead6686166bec6cb43b68be6d608c922a42f47526c637de56c670b077750fa0a87359af2'
