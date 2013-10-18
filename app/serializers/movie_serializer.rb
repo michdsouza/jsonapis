69c49d4e3cddc92654fc68c088e04cbe9dbcdc2f
Eric Chernuka <ericchernuka@gmail.com>
2013-10-17 21:41:36 -0600

    Associations! Conforming to the JSON format of APIs in order to establish predictability and consistency at jsonapi.org.

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,11 +1,16 @@
 class MovieSerializer < ActiveModel::Serializer
+  embed :ids, include: true
 
   attributes :id, :title, :synopsis, :runtime, :average_rating
 
-  has_many :ratings, embed: :object
-  has_one :genre, embed: :object
+  has_many :ratings
+  has_one :genre
 
   def average_rating
     object.average_rating
+  end
+
+  def ratings
+    object.ratings.select { |rating| rating.user == current_user }
   end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :full_name
+  attributes :id, :full_name
 end
