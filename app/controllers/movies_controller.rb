3d9a71200a24418ce6685cc7ba17e3ee400e716a
Michelle D'Souza <michd2005@gmail.com>
2013-10-21 18:39:30 -0600

    Use commented out code for step by step

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,13 +3,33 @@ class MoviesController < ApplicationController
 
 	def index
 		@movies = Movie.all
-      render :json => @movies.to_json(
-        :only => [:title, :synopsis, :runtime],
-        :include => {
-       		:genre => { :only => [:name] },
-          :ratings => { :only => [:rating], :include => [:user => {:only => [], :methods => [:full_name]}] }
-        },
-        :methods => [:average_rating]
-        )
+    
+    ## 1
+    render :json => @movies.to_json
+    
+    ## 2
+    # render :json => @movies.to_json(:only => [:id, :title, :synopsis, :runtime])
+
+    ## 3
+    # render :json => @movies.to_json(:only => [:id, :title, :synopsis, :runtime],
+    #     :include => {
+    #       :genre => { :only => [:name] }
+    #     })
+
+    ## 4
+    # render :json => @movies.to_json(:only => [:id, :title, :synopsis, :runtime],
+    #     :include => {
+    #       :genre => { :only => [:name] }
+    #     },
+    #     :methods => [:average_rating])
+    
+    ## 5
+    # render :json => @movies.to_json(:only => [:id, :title, :synopsis, :runtime],
+    #     :include => {
+    #    		:genre => { :only => [:name] },
+    #       :ratings => { :only => [:rating], :include => [:user => {:only => [], :methods => [:full_name]}] }
+    #     },
+    #     :methods => [:average_rating]
+    #   )
 	end
 end
