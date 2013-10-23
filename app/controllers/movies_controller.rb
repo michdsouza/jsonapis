61ca20e461842f9dd12709e555556c6d859d8543
Eric Chernuka <ericchernuka@gmail.com>
2013-10-23 08:20:55 -0600

    Uncommented code for final result.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,6 @@ class MoviesController < ApplicationController
 
 	def index
 		@movies = Movie.includes(:genre, :ratings => [:user]).all
-    render json: @movies, root: false
+    render json: @movies
 	end
 end
--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,66 +1,16 @@
 class MovieSerializer < ActiveModel::Serializer
-  ## 1
-  # attributes :id, :title, :synopsis, :runtime, :average_rating
+  embed :ids, include: true
 
-  ## 2
-  # attributes :id, :title, :synopsis, :runtime, :average_rating
+  attributes :id, :title, :synopsis, :runtime, :average_rating
 
-  # has_many :ratings
-  # has_one :genre
+  has_many :ratings, key: :rating_ids, root: :rating_objects
+  has_one :genre
 
-  ## 3
-  # attributes :id, :title, :synopsis, :runtime, :average_rating
+  def average_rating
+    object.average_rating
+  end
 
-  # has_many :ratings
-  # has_one :genre
-
-  # def average_rating
-  #   object.average_rating
-  # end
-
-  ## 4
-  # attributes :id, :title, :synopsis, :runtime, :average_rating
-
-  # has_many :ratings
-  # has_one :genre
-
-  # def average_rating
-  #   object.average_rating
-  # end
-
-  # def ratings
-  #   object.ratings.select { |rating| rating.user == current_user }
-  # end
-
-  ## 5
-  # embed :ids, include: true
-
-  # attributes :id, :title, :synopsis, :runtime, :average_rating
-
-  # has_many :ratings
-  # has_one :genre
-
-  # def average_rating
-  #   object.average_rating
-  # end
-
-  # def ratings
-  #   object.ratings.select { |rating| rating.user == current_user }
-  # end
-
-  ## 6
-  # embed :ids, include: true
-
-  # attributes :id, :title, :synopsis, :runtime, :average_rating
-
-  # has_many :ratings, key: :rating_ids, root: :rating_objects
-  # has_one :genre
-
-  # def average_rating
-  #   object.average_rating
-  # end
-
-  # def ratings
-  #   object.ratings.select { |rating| rating.user == current_user }
-  # end
+  def ratings
+    object.ratings.select { |rating| rating.user == current_user }
+  end
 end
