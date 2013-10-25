5a4bcf1056774283dc557c35a682ccbcc6f768bf
Michelle D'Souza <michd2005@gmail.com>
2013-10-24 22:27:56 -0600

    Removed comments from movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,33 +3,12 @@ class MoviesController < ApplicationController
 
 	def index
 		@movies = Movie.all
-    
-    ## 1
-    render :json => @movies.to_json
-    
-    ## 2
-    # render :json => @movies.to_json(:only => [:id, :title, :synopsis, :runtime])
-
-    ## 3
-    # render :json => @movies.to_json(:only => [:id, :title, :synopsis, :runtime],
-    #     :include => {
-    #       :genre => { :only => [:name] }
-    #     })
-
-    ## 4
-    # render :json => @movies.to_json(:only => [:id, :title, :synopsis, :runtime],
-    #     :include => {
-    #       :genre => { :only => [:name] }
-    #     },
-    #     :methods => [:average_rating])
-    
-    ## 5
-    # render :json => @movies.to_json(:only => [:id, :title, :synopsis, :runtime],
-    #     :include => {
-    #    		:genre => { :only => [:name] },
-    #       :ratings => { :only => [:rating], :include => [:user => {:only => [], :methods => [:full_name]}] }
-    #     },
-    #     :methods => [:average_rating]
-    #   )
+    render :json => @movies.to_json(:only => [:id, :title, :synopsis, :runtime],
+        :include => {
+       		:genre => { :only => [:name] },
+          :ratings => { :only => [:rating], :include => [:user => {:only => [], :methods => [:full_name]}] }
+        },
+        :methods => [:average_rating]
+      )
 	end
 end
